51f1893b5aaea47385ff154263f7d242567b94aa
AnaRaus <raus.ana9@gmail.com>
2022-10-19 20:14:10 +0300

    Ana Răus: am adăugat 3 clase

---
+++ classes/pachet_ore.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import relationship
+from base import Base
+from sqlalchemy import Column, String, Integer, Date, Table, ForeignKey
+
+
+class PachetOre(Base):
+    __tablename__ = 'pachet_ore'
+
+    id = Column(Integer, primary_key=True)
+    durata = Column(String)
+    instructor_id = Column(Integer, ForeignKey("instructor.id"))
+    instructor = relationship("Instructor", back_populates="pachet_ore")
+
+    def __init__(self, durata):
+        self.durata =
---
+++ classes/personal.py
@@ -0,0 +1,18 @@
+from sqlalchemy.orm import relationship
+from base import Base
+from sqlalchemy import Column, String, Integer, Date, Table, ForeignKey
+
+
+class Personal(Base):
+    __tablename__ = 'personal'
+
+    id = Column(Integer, primary_key=True)
+    nume = Column(String)
+    prenume = Column(String)
+    cont = relationship("Cont", back_populates="personal")
+    cont_id = Column(Integer, ForeignKey("cont.id"))
+    instructor = relationship("Instructor", back_populates="personal")
+
+    def __init__(self, nume, prenume):
+        self.nume = nume
+        self.prenume = prenume
