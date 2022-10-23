1b74d5be43d39a80e27710cad366c9184ec9a610
CosminHongu <cosmin513@gmail.com>
2022-10-23 20:31:13 +0300

    Adaugare relatie in clasa Cursant dintre pachet_ore si cursant.

--- classes/base.py
+++ classes/base.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 engine = create_engine('postgresql://CAIA:delta1@localhost:5432/CAIA1')
 
+#Creare conexiune noua la baza de date
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
