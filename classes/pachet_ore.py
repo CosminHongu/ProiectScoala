eaec5991e6b42a9a4d46fe5293a3ac7e00888103
AnaRaus <raus.ana9@gmail.com>
2022-11-07 23:16:21 +0200

    Ana Răus: Modificare: am ad[ugat notnull și max pentru unele coloane

--- classes/pachet_ore.py
+++ classes/pachet_ore.py
@@ -11,13 +11,13 @@ class PachetOre(Base):
     __tablename__ = 'pachet_ore'
 #MANY TO MANY
     instructor_pachet_ore_relationship = Table('instructor_pachet_ore_relationship', Base.metadata,
-                                    Column('instructor_id', Integer, ForeignKey('instructor.id')),
-                                    Column('pachet_ore_id', Integer, ForeignKey('pachet_ore.id')),
+                                    Column('instructor_id', Integer(100), ForeignKey('instructor.id')),
+                                    Column('pachet_ore_id', Integer(100), ForeignKey('pachet_ore.id')),
 
                                     )
 
-    id = Column(Integer, primary_key=True)
-    durata = Column(Integer)
+    id = Column(Integer(100), primary_key=True)
+    durata = Column(Integer(100), nullable=False)
     instructor = relationship('Instructor', secondary=instructor_pachet_ore_relationship)
 
     def __init__(self, durata):
