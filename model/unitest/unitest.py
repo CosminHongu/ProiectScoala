a83805101cfcfd9fff459376ef347edf423f86c1
CosminHongu <cosmin513@gmail.com>
2022-11-08 18:25:19 +0200

    Libraria Unitest poate fi folosita pentru Teste - de revenit pe acest aspect diseara

---
+++ model/unitest/unitest.py
@@ -0,0 +1,14 @@
+import unittest
+from base import Session, Base, engine
+from cont import Cont
+from cursant import Cursant
+from instructor import Instructor
+from pachet_ore import PachetOre
+
+from personal import Personal
+from personal_administrativ import PersonalAdministrativ
+from programare import Programare
+from sediu import Sediu
+from address import Address
+from vehicul import Vehicul
+
